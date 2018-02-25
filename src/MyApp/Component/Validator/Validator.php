6d172c31da35a06f040f09b41576148158aaf1b9
Daniel Salgado Población <daniel.salgado.p@gmail.com>
2018-02-25 16:30:29 +0100

    small changes

--- src/MyApp/Component/Validator/Validator.php
+++ src/MyApp/Component/Validator/Validator.php
@@ -4,9 +4,16 @@ namespace MyApp\Component\Validator;
 class Validator
 {
 
+    /**
+     * Everything receibed are integer numbers
+     *
+     * @param      array    of values
+     *
+     * @return     boolean
+     */
     public function areNumbers(array $arr_param){
         foreach ($arr_param as $value) {
-        if (! filter_var($value, FILTER_VALIDATE_INT)) {
+            if (! filter_var($value, FILTER_VALIDATE_INT)) {
                 return false;
             }
         }
