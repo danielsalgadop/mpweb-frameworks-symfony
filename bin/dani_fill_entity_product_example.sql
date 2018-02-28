cd88af0c5db35f130e046e6643cfe02d04f9a302
daniel salgado <daniel.salgado.p@gmail.com>
2018-03-01 00:58:36 +0100

    got delete product

---
+++ bin/dani_fill_entity_product_example.sql
@@ -0,0 +1,38 @@
+USE symfony;
+TRUNCATE TABLE  product;
+
+insert into product( name, price, description) values
+    ('name1', '1', 'description1'),
+    ('name2', '2', 'description2'),
+    ('name3', '3', 'description3'),
+    ('name4', '4', 'description4'),
+    ('name5', '5', 'description5'),
+    ('name6', '6', 'description6'),
+    ('name7', '7', 'description7'),
+    ('name8', '8', 'description8'),
+    ('name9', '9', 'description9'),
+    ('name10', '10', 'description10'),
+    ('name11', '11', 'description11'),
+    ('name12', '12', 'description12'),
+    ('name13', '13', 'description13'),
+    ('name14', '14', 'description14'),
+    ('name15', '15', 'description15'),
+    ('name16', '16', 'description16'),
+    ('name17', '17', 'description17'),
+    ('name18', '18', 'description18'),
+    ('name19', '19', 'description19'),
+    ('name20', '20', 'description20'),
+    ('name21', '21', 'description21'),
+    ('name22', '22', 'description22'),
+    ('name23', '23', 'description23'),
+    ('name24', '24', 'description24'),
+    ('name25', '25', 'description25'),
+    ('name26', '26', 'description26'),
+    ('name27', '27', 'description27'),
+    ('name28', '28', 'description28'),
+    ('name29', '29', 'description29')
+
+
+    ;
+
+
--- src/MyApp/Bundle/AppBundle/Controller/DeleteProductController.php
+++ src/MyApp/Bundle/AppBundle/Controller/DeleteProductController.php
@@ -3,7 +3,43 @@
 namespace MyApp\Bundle\AppBundle\Controller;
 
 
-class DeleteProductController
+use Doctrine\ORM\Query;
+use MyApp\Bundle\AppBundle\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class DeleteProductController extends Controller
 {
+    public function executeAction()
+    {
+        // $product = $this->getDoctrine()
+        //      ->getRepository(Product::class)
+        //      ->find($productId);
+
+        $entity_manager  = $this->getDoctrine()->getManager();
+        $product = $entity_manager
+             ->getReference('\MyApp\Bundle\AppBundle\Entity\Product',1);
+        $entity_manager->remove($product);
+        $entity_manager->flush();
+        return new JsonResponse(['dani','crack']);
+
+        /*$products = $this->getDoctrine()->getRepository('\MyApp\Bundle\AppBundle\Entity\Product')->findAll(Query::HYDRATE_ARRAY);
+
+        $productsAsArray = array_map(function (Product $p) {
+            return $this->productToArray($p);
+        }, $products);
+
+        return new JsonResponse($productsAsArray);*/
+    }
+
+/*    private function productToArray(Product $product)
+    {
+        return [
+            'id' => $product->getId(),
+            'name' => $product->getName(),
+            'price' => $product->getPrice(),
+        ];
+    }
+*/
 
 }
