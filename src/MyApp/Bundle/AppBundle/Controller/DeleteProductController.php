79891c20c8c61217519b3bb58fe1c4cd34b4f1fe
daniel salgado <daniel.salgado.p@gmail.com>
2018-03-01 11:37:28 +0100

    another way delete product, detecting if product exists

--- src/MyApp/Bundle/AppBundle/Controller/DeleteProductController.php
+++ src/MyApp/Bundle/AppBundle/Controller/DeleteProductController.php
@@ -10,18 +10,37 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DeleteProductController extends Controller
 {
-    public function executeAction()
+    public function executeAction($id)
     {
-        // $product = $this->getDoctrine()
-        //      ->getRepository(Product::class)
-        //      ->find($productId);
+        $product = $this->getDoctrine()
+             ->getRepository(Product::class)
+             ->findOneById($id);
 
         $entity_manager  = $this->getDoctrine()->getManager();
-        $product = $entity_manager
-             ->getReference('\MyApp\Bundle\AppBundle\Entity\Product',1);
+
+        // $product = $entity_manager
+             // ->getReference('\MyApp\Bundle\AppBundle\Entity\Product',$id);
+        // $product = $entity_manager->findOneById($id);
+             // file_put_contents("/home/dan/filelogs.log",var_export($product,true),FILE_APPEND);
+
+
+
+   if (!$product) {
+        return new JsonResponse(['status','error','message','product not found']);
+
+        // throw $this->createNotFoundException(
+        //     'No product found for id '.$productId
+        // );
+    }
+
+
         $entity_manager->remove($product);
         $entity_manager->flush();
         return new JsonResponse(['dani','crack']);
+
+
+
+
 
         /*$products = $this->getDoctrine()->getRepository('\MyApp\Bundle\AppBundle\Entity\Product')->findAll(Query::HYDRATE_ARRAY);
 
